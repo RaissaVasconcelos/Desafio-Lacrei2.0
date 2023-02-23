e4ffe83ba0d757607045920b89cd10ec16f0784d
Raissa Vasconcelos mendes <rai.celos20@gmail.com>
2023-02-23 19:21:27 -0300

    feat: create Home component

---
+++ desafio-lacrei/src/componentes/Home.tsx
@@ -0,0 +1,20 @@
+import image from '../../public/rafiki 2.svg'
+import { Main, ButtonContainer, SectionText } from './Home.styled'
+
+export function Home() {
+  return (
+    <Main>
+      <SectionText>
+        <h1>Boas vindas a Lacrei Saúde</h1>
+        <p>Uma plataforma segura e acolhedora para comunidade LGBTQIAPN+</p>
+        <ButtonContainer>
+          <button>Pessoa Usuária</button>
+          <button>Pessoa Profissional</button>
+        </ButtonContainer>
+      </SectionText>
+      <section>
+        <img src={image} alt='Imagem de Profissionais'/>
+      </section>
+    </Main>
+  )
+}
