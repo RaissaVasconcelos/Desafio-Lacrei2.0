56ab13d6fa194c1973e470b776bfdc28afed0165
Raissa Vasconcelos mendes <rai.celos20@gmail.com>
2023-02-25 18:15:23 -0300

    style: create Person Component

---
+++ desafio-lacrei/src/componentes/Person/Person.style.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const Main = styled.main`
+  display: flex;
+  gap: 32px;
+`
+
+export const SectionText = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+`
+
